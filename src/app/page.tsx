98d7fd35d53bd036aad904acce812594e52deb6a
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-09-07 12:41:39 +0900

    fix: Link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -75,7 +75,7 @@ const StudentHomePage = () => {
         </Link>
       </div>
 
-      <Link href="/guide" className="mt-12 text-indigo-600 hover:text-indigo-800 transition-colors">
+      <Link href="/guide" target="_blank" rel="noopener noreferrer" className="mt-12 text-indigo-600 hover:text-indigo-800 transition-colors">
         <span className="text-sm">アプリの使い方ページはこちら</span>
       </Link>
       
