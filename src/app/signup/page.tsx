e519c6dd680cdc91d33a91426aed2f0a1c3d77b9
Shunsuke0205 <hirata-shunsuke1125@g.ecc.u-tokyo.ac.jp>
2025-09-08 10:52:34 +0900

    impl: /signup

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,61 @@
+import Link from 'next/link';
+import { signup } from '../login/actions'
+
+export default function SignupPage() {
+  return (
+    <div className="h-screen flex items-start justify-center bg-gray-100 p-4">
+      <div className="w-full max-w-md bg-white rounded-xl shadow-lg p-8 space-y-6">
+        <h1 className="text-3xl font-bold text-center text-gray-800">新規登録</h1>
+
+
+        {/* サインアップフォーム */}
+        <form className="space-y-4">
+          <div>
+            <label htmlFor="email" className="block text-sm font-medium text-gray-700">
+              メールアドレス
+            </label>
+            <input
+              id="email"
+              name="email"
+              type="email"
+              required
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm p-2 text-gray-900 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            />
+          </div>
+          <div>
+            <label htmlFor="password" className="block text-sm font-medium text-gray-700">
+              パスワード
+            </label>
+            <input
+              id="password"
+              name="password"
+              type="password"
+              required
+              className="mt-1 block w-full rounded-md border-gray-300 shadow-sm p-2 text-gray-900 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            />
+          </div>
+          <button
+            formAction={signup}
+            className="w-full py-3 px-4 bg-indigo-600 text-white font-semibold rounded-lg shadow-md hover:bg-indigo-700 transition duration-300 ease-in-out"
+          >
+            新規登録
+          </button>
+        </form>
+
+        {/* ログインへの案内 */}
+        <div className="mt-14">
+          <p className="text-sm">
+            すでにアカウントをつくっていらっしゃいますか？
+          </p>
+          <Link href="/login">
+            <button className="mt-4 w-full py-2 px-4 border border-indigo-600 text-indigo-600 font-semibold rounded-lg hover:bg-indigo-50 transition duration-300 ease-in-out">
+              ログインはこちら
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
